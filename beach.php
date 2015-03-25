530637c744d518d0393e36244a2e01ec957af3ea
Selena <selenamo@umich.edu>
2015-03-25 18:24:27 -0400

    updated beach php

--- beach.php
+++ beach.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" type="text/css" href="styles.css">
 	<link rel="stylesheet" type="text/css" href="beachstyles.css">
 	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-h</head>
+</head>
 
 	<body>
 	<?php
@@ -16,17 +16,18 @@ h</head>
 	?>
 		<h1>Panama City Beach, Florida</h1>
 
-<div id="summary">
+<div id="top-photos">
+        <ul>
+            <li><img src="http://www.rabeachgetaways.com/images/gallery/springbreak-gallery6.jpg" alt= "pcbresort"></a></li>          
+            <li><img src="http://www.mtvu.com/wp-content/gallery/2009/springbreak/fans/_AP_0691.JPG" alt= "pcbrealness"></a></li>          
+            <li><img src="http://www.laquintapcb.com/wp-content/sdaolpu/2011/04/peirpark1.jpg" alt= "nighttime"></a></li>          
 
-	<li> <h2> World Class Spring Break nightclubs and with Spinnaker, Club La Vela and Sharkeys </h2> </li>
-	<li> <h2> Experience the Spring Break party scene on sugary white sand beaches </h2> </li>
-	<li> <h2> Pool parties, live concerts on the beach </h2> </li>
+        </ul>
 </div>
-
 </br>
 
 
-
+<div id="summary">
 		<h2>Located on the warm waters of the Gulf of Mexico,
 		 there are 27 miles of sugary white sand beaches where
 		drinking on the beach is legal. What else could a spring 
@@ -37,11 +38,12 @@ h</head>
 		 Spinnaker, and the largest nightclub in the U.S., Club La Vela.
 		Lay out and get a tan, parasail, snorkel for starfish in clear
 		blue waters or jetski right in front of your hotel in PCB, Florida. </h2>
+</div>
 
+</br>
+</br>
+</br>
 
-        <ul>
-            <li><img src="http://www.rabeachgetaways.com/images/gallery/springbreak-gallery6.jpg" height = "250" alt= "pcbresort"></a></li>          
-        </ul>
 	<?php
 			include "footer.php" ;
 	?>
