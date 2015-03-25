c7eff641e63fbf07f294abf7ca7d84c00a8d5050
Lulu Tang <lulu@fetchnotes.com>
2015-03-25 17:05:52 -0400

    fix some styling on contact form

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <div id="foodcontent">
 <div id="foodGallery" class="gallery class">
         <ul>
-            <li><a href="pcb.php"><img src="http://beachsideresortpanamacitybeach.com/wp-content/gallery/home/balcony_view_beach_pool_0.jpg" alt="pcb"></a></li>
+            <li><a href="beach.php"><img src="http://beachsideresortpanamacitybeach.com/wp-content/gallery/home/balcony_view_beach_pool_0.jpg" alt="pcb"></a></li>
             <li><a href="aspen.php"><img src="http://aspenalps.com/wp-content/uploads/2012/10/aspen-colorado-snowmass.jpg" alt="aspen"></a></li>
             <li><a href="seattle.php"><img src="http://jamesb.com/wp-content/uploads/2012/05/seattleSkyline.jpg" alt= 
                 "seattle"></a></li>            
