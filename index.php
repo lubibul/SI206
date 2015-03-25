7ecc22b2358d0accbfb3f28f9e988d0433c62963
Lulu Tang <lulu@fetchnotes.com>
2015-03-25 17:52:43 -0400

    saved pictures for front page

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
 
 	<div id="top-photos">
 		<ul>
-		    <li><a href="beach.php"><img src="http://beachsideresortpanamacitybeach.com/wp-content/gallery/home/balcony_view_beach_pool_0.jpg" alt="pcb" /></a>
-		    </li><li><a href="aspen.php"><img src="http://aspenalps.com/wp-content/uploads/2012/10/aspen-colorado-snowmass.jpg" alt="aspen" /></a>
-			</li><li><a href="seattle.php"><img src="http://jamesb.com/wp-content/uploads/2012/05/seattleSkyline.jpg" alt= "seattle" /></a></li>
+		    <li><a href="beach.php"><img src="img/pcb-1.png" alt="pcb" /></a>
+		    </li><li><a href="aspen.php"><img src="img/aspen-1.png" alt="aspen" /></a>
+			</li><li><a href="seattle.php"><img src="img/seattle-1.png" alt= "seattle" /></a></li>
 		</ul>
 	</div>
 
