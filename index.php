295413c0105f44a7981b1ff241320ee5b7589dba
Selena <selenamo@umich.edu>
2015-03-25 00:34:51 -0400

    updated index.php

--- index.php
+++ index.php
@@ -8,14 +8,24 @@
 	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
-	<body>
+<body>
 		<?php include 'header.php'; ?>
 		
 		<h1>This is our awesome travel agency</h1>
 		<p>Some text here</p>
 
-		<footer>
-			Travel Agency Site &copy; 2015. Created by Ben Beale, Selena Moeljadi, and Lulu Tang.
-		</footer>
-	</body>
+<div id="foodcontent">
+<div id="foodGallery" class="gallery class">
+        <ul>
+            <li><a href="pcb.php"><img src="http://beachsideresortpanamacitybeach.com/wp-content/gallery/home/balcony_view_beach_pool_0.jpg" alt="pcb"></a></li>
+            <li><a href="aspen.php"><img src="http://aspenalps.com/wp-content/uploads/2012/10/aspen-colorado-snowmass.jpg" alt="aspen"></a></li>
+            <li><a href="seattle.php"><img src="http://jamesb.com/wp-content/uploads/2012/05/seattleSkyline.jpg" alt= 
+                "seattle"></a></li>            
+        </ul>
+
+</body>
+
+<footer>
+	Travel Agency Site &copy; 2015. Created by Ben Beale, Selena Moeljadi, and Lulu Tang.
+</footer>
 </html>
