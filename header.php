9e651b7f15777fa8348a30d47a109486be753a87
Lulu Tang <lulu@fetchnotes.com>
2015-04-02 17:56:14 -0400

    added the nav bar to header.php

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
 <?php
 	echo "<header><a href=\"index.php\"><img src='img/logo.png' alt='company logo'/></a></header>";
+	echo "<nav>
+		<a href=\"index.php\">Home</a>
+		<a href=\"contact.php\">Book Now</a>
+		<a href=\"beach.php\">PCB</a>
+		<a href=\"aspen.php\">Aspen</a>
+		<a href=\"seattle.php\">Seattle</a>
+		<a href=\"about.php\">About</a>
+	</nav>";
 ?>
--- index.php
+++ index.php
@@ -12,43 +12,32 @@
 <body>
 	<?php include 'header.php'; ?>
 
-	<nav>
-		<a href="index.php">Home</a>
-		<a href="contact.php">Book Now</a>
-		<a href="beach.php">PCB</a>
-		<a href="aspen.php">Aspen</a>
-		<a href="seattle.php">Seattle</a>
-		<a href="about.php">About</a>
-	</nav>
-
-	<!-- <div id="top-photos">
-		<ul>
-		    <li><a href="beach.php"><img src="img/pcb-1.png" alt="pcb" /></a>
-		    </li><li><a href="aspen.php"><img src="img/aspen-1.png" alt="aspen" /></a>
-			</li><li><a href="seattle.php"><img src="img/seattle-1.png" alt= "seattle" /></a></li>
-		</ul>
-	</div> -->
 	<section>
 		<h1>Welcome to Travel Agency!</h1>
-		<!-- <p><a href="contact.php">Book Your Trip Now!</a></p> -->
 	</section>
 	<section class="card-container">
 		<a href="beach.php">
 			<div class="card">
 				<h1>PCB, FL</h1>
 				<img src="img/pcb-1.png" alt="pcb" />
+				<p>Get away to the endless sandy beaches of Panama City Beach, 
+					where the parties are just as endless and the excitement just as crazy.</p>
 			</div>
 		</a>
 		<a href="aspen.php">
 			<div class="card">
 				<h1>Aspen, CO</h1>
-				<img src="img/aspen-1.png" alt="pcb" />
+				<img src="img/aspen-1.png" alt="aspen" />
+				<p>Spring break has never been cooler than at Aspen.
+					Go wild with these beautiful mountains and fresh air!</p>
 			</div>
 		</a>
 		<a href="seattle.php">
 			<div class="card">
 				<h1>Seattle, WA</h1>
-				<img src="img/seattle-1.png" alt="pcb" />
+				<img src="img/seattle-1.png" alt="seattle" />
+				<p>Ever wanted to be right in the middle of the hustle and bustle?
+					Seattle is the place for you!</p>
 			</div>
 		</a>	
 	</section>
